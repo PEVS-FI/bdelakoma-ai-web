4454de9fae8931e6041bcc0fd9ccf0ecd73fa121
Michal Drobny <michal.drobny@me.com>
2026-01-07 20:11:06 +0100

    Partner update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,8 +39,10 @@
                 <div class="Gift-carousel owl-carousel">
                     <a href="https://paneurouni.com" target="_blank"><img src="{{ asset('images/partners/pevs.png') }}" lazy alt="Paneurópska vysoká škola"></a>
                     <a href="https://www.nadaciatatrabanky.sk" target="_blank"><img src="{{ asset('images/partners/ttb.jpg') }}" lazy alt="Nadácia Tatra Banky"></a>
-                    <a href="https://www.nadaciavub.sk" target="_blank"><img src="{{ asset('images/partners/vub.png') }}" lazy alt="Nádácia VÚB"></a>
-                    <a href="https://sk-nic.sk/pt/fond-sk-nic/" target="_blank"><img src="{{ asset('images/partners/sknic.png') }}" lazy alt="Fond SK-NIC"></a>
+                    <a href="https://www.nadaciavub.sk" target="_blank"><img style="width: 95% !important; margin-top: 25%" src="{{ asset('images/partners/nadacia_vub.png') }}" lazy alt="Nádácia VÚB"></a>
+                    <a href="https://www.vub.sk" target="_blank"><img src="{{ asset('images/partners/vub.png') }}" lazy alt="VÚB Banka"></a>
+                    <a href="https://sk-nic.sk/pt/fond-sk-nic/" target="_blank"><img style="width: 85% !important; margin-top: 5%" src="{{ asset('images/partners/sknic.png') }}" lazy alt="Fond SK-NIC"></a>
+                    <a href="https://sk-nic.sk/" target="_blank"><img style="width: 85% !important; margin-top: 20%" src="{{ asset('images/partners/sknic-logo.png') }}" lazy alt="SK-NIC"></a>
                 </div>
             </div>
         </div>
